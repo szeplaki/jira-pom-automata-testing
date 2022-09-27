146da0ea1085ec0323d729111c7d03ecb06f6e85
BghBenja <bghbenja@gmail.com>
2022-09-27 12:36:07 +0200

    Correct browseIssueWithSpecificId

--- src/test/java/BrowseIssue/BrowseIssueTest.java
+++ src/test/java/BrowseIssue/BrowseIssueTest.java
@@ -71,7 +71,8 @@ public class BrowseIssueTest {
     public void browseIssueWithSpecificId(String issueId) {
 
         webDriver.get(String.format("https://jira-auto.codecool.metastage.net/browse/%s", issueId));
-        Assertions.assertDoesNotThrow(() -> Assertions.assertEquals(issueId, browseIssueModel.getIssueId()));
+        Assertions.assertDoesNotThrow(() -> browseIssueModel.getIssueId());
+        Assertions.assertEquals(issueId, browseIssueModel.getIssueId());
 
 
     }
