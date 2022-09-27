2848ec27e2e78944fcf1373a3f2aa7f814aca608
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-09-27 10:36:26 +0200

    Implement waitUntilVisible method

--- src/main/java/com/codecool/RandomHelper.java
+++ src/main/java/com/codecool/RandomHelper.java
@@ -1,9 +1,13 @@
 package com.codecool;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class RandomHelper {
-    // TODO this is the old wait, Zsoli wants to replace it
     public static void Wait(WebDriver webDriver){
         synchronized (webDriver){
             try {
@@ -11,6 +15,27 @@ public class RandomHelper {
             }catch (InterruptedException e){
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    public static void waitUntilVisible(WebDriver webDriver, String elementType, String elementId){
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+
+        switch (elementType){
+            case "id":
+                wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.id(elementId))));
+                break;
+            case "xpath":
+                wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.xpath(elementId))));
+                break;
+            case "className":
+                wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.className(elementId))));
+                break;
+            case "css":
+                wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.cssSelector(elementId))));
+                break;
+            default:
+                RandomHelper.Wait(webDriver);
         }
     }
 }
