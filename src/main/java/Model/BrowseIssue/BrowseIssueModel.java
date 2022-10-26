187861f48b10d4b5c79138e16b993e1f83e9a075
Szeplaki <szeplaki.eva1@gmail.com>
2022-10-26 20:43:29 +0200

    correct issueId in baseIssueModel

--- src/main/java/Model/BrowseIssue/BrowseIssueModel.java
+++ src/main/java/Model/BrowseIssue/BrowseIssueModel.java
@@ -20,7 +20,7 @@ public class BrowseIssueModel extends BaseModel {
         this.driverWait = new WebDriverWait(webDriver, Duration.ofSeconds(15));
     }
 
-    @FindBy(id = "key-val")
+    @FindBy(css = "#key-val")
     private WebElement issueId;
 
     @FindBy(id = "searcher-query")
